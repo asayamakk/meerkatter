d0e008db305623d1e9232e248ea60a1d980a5310
浅山広大 <kodai419419@gmail.com>
2016-07-19 23:25:32 +0900

    awful fix for gem GoogleImegeFethcer

--- app.rb
+++ app.rb
@@ -1,3 +1,15 @@
+class CLI
+  def search(query)
+    search_url = Fetcher.search_url(query)
+    conn = Faraday.new(url: URI.encode(search_url))
+    response = conn.get
+    result = JSON.parse(response.body)
+
+    image_url = result["items"].map {|item| item["link"] }
+    # Fetcher.store(query, image_url)
+  end
+end
+
 require 'slack-ruby-client'
 require 'google_image_fetcher'
 require 'active_support'
