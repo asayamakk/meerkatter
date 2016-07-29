681ef9d0fc4b732115f68787819675b5154ab49a
Kodai Asayama <akkdkb@gmail.com>
2016-07-29 12:12:59 +0900

    橋本環奈を追加

--- app.rb
+++ app.rb
@@ -42,6 +42,8 @@ client.on :message do |data|
     search_word = (rand < 0.95 ? 'ミーアキャット' : 'キツネザル') + message.presence.to_s
     if message.include?('パンダ')
       search_word = message
+    elsif message.include?('橋本環奈')
+      search_word = message
     end
     image_list = GoogleImageFetcher::CLI.new.search(search_word)
     image_url = image_list.sample
